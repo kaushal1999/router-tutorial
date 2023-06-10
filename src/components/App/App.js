cc1d0eb7f09d7c17bf66f033b974d16de99ee61a
kaushal1999 <kaushalwaghela1999@gmail.com>
2023-06-10 12:27:43 +0530

    added react router & routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,37 @@
-import React, { Component } from 'react';
-import './App.css';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Manatee from '../Manatee/manatee';
+import React, { Component } from "react";
+import "./App.css";
+import { Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Manatee from "../Manatee/manatee";
+import Whale from "../Whale/Whale";
+import Narwhal from "../Narwhal/Narwhal";
 
 class App extends Component {
   render() {
     return (
-      <div className='wrapper'>
+      <div className="wrapper">
         <h1>Marine Mammals</h1>
-        <Manatee></Manatee>
+
+        <BrowserRouter>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/manatee">Manatee</Link>
+              </li>
+              <li>
+                <Link to="/whale">Whale</Link>
+                <li>
+                  <Link to="/narwhal">Narwhal</Link>
+                </li>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/whale" element={<Whale></Whale>} />
+            <Route path="/narwhal" element={<Narwhal></Narwhal>} />
+            <Route path="/manatee" element={<Manatee></Manatee>} />
+          </Routes>
+        </BrowserRouter>
       </div>
     );
   }
